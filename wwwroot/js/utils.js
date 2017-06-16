fdc884a93db1570c14c413047773db1981d3c281
Carles Capellas <carlesc@byrom.plc.uk>
2017-06-16 11:09:20 +0200

    Improve loader animation

--- wwwroot/js/utils.js
+++ wwwroot/js/utils.js
@@ -6,7 +6,13 @@
     window.application.utils = {
         longOperation: function (promiseBuilder, loader) {
             loader.fadeIn().promise().done(function() {
+                loader.parent().css({
+                    height: 0
+                });
                 return promiseBuilder().always(function() {
+                    loader.parent().css({
+                        height: 'auto'
+                    });
                     return loader.delay(400).fadeOut().promise();
                 });
             });
