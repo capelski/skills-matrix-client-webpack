8241b0ffa4b13ef4df56522d0020b3a44b8bb15f
Carles Capellas <carlesc@byrom.plc.uk>
2017-06-28 14:14:51 +0200

    Replace home page search-list with actions and updaters in order to fit the Model View Update pattern

--- todo.md
+++ todo.md
@@ -1,7 +1,5 @@
-- Refactor Search-List to fit the Elm Architecture Pattern
-    - Replace search-list in indexes; Remove it
-    
 - Que es pugui clicar les creus a Edit
+- Que es puguin clicar els li enlloc de només la <a>
 - Lists Pagination
 - Fade In/Out when loading/leaving
 - Include zoom icon in searcher and cross to delete the text
--- wwwroot/js/home/index.js
+++ wwwroot/js/home/index.js
@@ -1,9 +1,8 @@
+window.application = window.application || {};
+window.application.home = window.application.home || {};
+    
+// View
 (function() {
-    var ajax = window.application.ajax;
-    var state = {
-        employees: [],
-        skills: []
-    };
     var htmlNodes = {
         employeesLoader : $('#employees-loader'),
         employeesList: $('#employees-list'),
@@ -12,41 +11,89 @@
         skillsList: $('#skills-list'),
         skillsKeywords: $('#skills-keywords')
     };
-    var employeesSearchList = new window.application.SearchList('employees', employeesPromise, {
-        elementDrawer: employeeDrawer
-    });
-    var skillsSearchList = new window.application.SearchList('skills', skillsPromise, {
-        elementDrawer: skillDrawer
-    });
+    var utils = window.application.utils;
 
-    employeesSearchList.reload();
-    skillsSearchList.reload();
-
-    function employeeDrawer(employee) {
-        return '<li class="list-group-item"><a class="reset" href="/employees/details?id=' + employee.Id + '">' + employee.Name +
-        '</a><span class="badge">' + employee.Skills.length + '</span></li>';
+    function update(state) {
+        for (var key in update) {
+            var updater = update[key];
+            updater(state);
+        }
     }
 
-    function employeesPromise(keywords) {
-        state.keywords = keywords;
-        return ajax.get('/api/employee/getMostSkilled')
-        .then(function(employees) {
-            state.employees = employees;
-            return employees;
+    update.employees = function (state) {
+        utils.fillList(htmlNodes.employeesList, state.employees, {
+            elementDrawer: function (employee) {
+                return '<li class="list-group-item"><a class="reset" href="/employees/details?id=' + employee.Id + '">' + employee.Name +
+                '</a><span class="badge">' + employee.Skills.length + '</span></li>';
+            },
+            noResultsHtml: '<i>No employees found</i>'
         });
-    }
+    };
 
-    function skillDrawer(skill) {
-        return '<li class="list-group-item"><a class="reset" href="/skills/details?id=' + skill.Id + '">' + skill.Name +
-        '</a><span class="badge">' + skill.Employees.length + '</span></li>';
-    }
-
-    function skillsPromise(keywords) {
-        state.keywords = keywords;
-        return ajax.get('/api/skill/getRearest')
-        .then(function(skills) {
-            state.skills = skills;
-            return skills;
+    update.skills = function (state) {
+        utils.fillList(htmlNodes.skillsList, state.skills, {
+            elementDrawer: function (skill) {
+                return '<li class="list-group-item"><a class="reset" href="/skills/details?id=' + skill.Id + '">' + skill.Name +
+                '</a><span class="badge">' + skill.Employees.length + '</span></li>';
+            },
+            noResultsHtml: '<i>No skills found</i>'
         });
+    };
+
+    window.application.home.htmlNodes = htmlNodes;
+    window.application.home.update = update;
+})();
+
+// Actions
+(function() {
+    var ajax = window.application.ajax;
+    var utils = window.application.utils;
+    var htmlNodes = window.application.home.htmlNodes;
+    var update = window.application.home.update;
+
+    function attachEvents(state) {
+        $().ready(utils.eventLinker(initializeView, state));
     }
+
+    function initializeView(state, event) {
+        _loadSkills(state);
+        _loadEmployees(state);
+    }
+
+    function _loadSkills(state) {
+        utils.longOperation(skillsPromise, htmlNodes.skillsLoader);
+
+        function skillsPromise() {
+            return ajax.get('/api/skill/getRearest', [])
+            .then(function(skills) {
+                state.skills = skills;
+                update.skills(state);
+            });
+        }
+    }
+
+    function _loadEmployees(state) {
+        utils.longOperation(employeesPromise, htmlNodes.employeesLoader);
+
+        function employeesPromise() {
+            return ajax.get('/api/employee/getMostSkilled', [])
+            .then(function(employees) {
+                state.employees = employees;
+                update.employees(state);
+            });
+        }
+    }
+
+    window.application.home.attachEvents = attachEvents;
+})();
+
+// Model
+(function() {
+    var state = {
+        employees: [],
+        skills: []
+    };
+
+    window.application.home.attachEvents(state);
+    window.application.home.state = state;
 })();
