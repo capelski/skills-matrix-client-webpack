2d61b88fb882dfafed86848bfea9ad8fb5abcc70
Carles Capellas <carlesc@byrom.plc.uk>
2017-06-28 12:22:24 +0200

    Replace the use of search-list with actions and updaters in both employee/skill list views in order to fit the Model Update View pattern

--- wwwroot/js/employees/list.js
+++ wwwroot/js/employees/list.js
@@ -1,28 +1,84 @@
+window.application = window.application || {};
+window.application.employeesList = window.application.employeesList || {};
+    
+// View
+(function() {
+    var htmlNodes = {
+        keywords : $('#employees-keywords'),
+        loader : $('#employees-loader'),
+        list : $('#employees-list')
+    };
+    var utils = window.application.utils;
+
+    function update(state) {
+        for (var key in update) {
+            var updater = update[key];
+            updater(state);
+        }
+    }
+
+    update.employees = function (state) {
+        utils.fillList(htmlNodes.list, state.employees, {
+            elementDrawer: function (employee) {
+                return '<li class="list-group-item"><a class="reset" href="/employees/details?id=' + employee.Id + '">' + employee.Name + '</a></li>';
+            },
+            noResultsHtml: '<i>No employees found</i>'
+        });
+    };
+
+    update.keywords = function (state) {
+        htmlNodes.keywords.val(state.keywords);
+    };
+
+    window.application.employeesList.htmlNodes = htmlNodes;
+    window.application.employeesList.update = update;
+})();
+
+// Actions
 (function() {
     var ajax = window.application.ajax;
+    var utils = window.application.utils;
+    var htmlNodes = window.application.employeesList.htmlNodes;
+    var update = window.application.employeesList.update;
+
+    function attachEvents(state) {
+        htmlNodes.keywords.on('keyup', utils.eventDelayer(utils.eventLinker(getEmployees, state)));
+        $().ready(utils.eventLinker(initializeView, state));
+    }
+
+    function getEmployees(state, event) {
+        state.keywords = event.target.value;
+        _loadEmployees(state);
+    }
+
+    function initializeView(state, event) {
+        _loadEmployees(state);
+    }
+
+    function _loadEmployees(state) {
+        utils.longOperation(employeesPromise, htmlNodes.loader);
+
+        function employeesPromise() {
+            return ajax.get('/api/employee?keywords=' + state.keywords + '&page=' + state.page + '&pageSize=' + state.pageSize, [])
+            .then(function(employees) {
+                state.employees = employees;
+                update.employees(state);
+            });
+        }
+    }
+
+    window.application.employeesList.attachEvents = attachEvents;
+})();
+
+// Model
+(function() {
     var state = {
         keywords: '',
         page: 0,
         pageSize: 10,
         employees: []
     };
-    var searchList = new window.application.SearchList('employees', employeesPromise, {
-        elementDrawer: employeeDrawer,
-        noResultsHtml: '<i>No employees found</i>'
-    });
 
-    searchList.reload();
-
-    function employeeDrawer(employee) {
-        return '<li class="list-group-item"><a class="reset" href="/employees/details?id=' + employee.Id + '">' + employee.Name + '</a></li>';
-    }
-
-    function employeesPromise(keywords) {
-        state.keywords = keywords;
-        return ajax.get('/api/employee?keywords=' + state.keywords + '&page=' + state.page + '&pageSize=' + state.pageSize, [])
-        .then(function(employees) {
-            state.employees = employees;
-            return employees;
-        });
-    }
+    window.application.employeesList.attachEvents(state);
+    window.application.employeesList.state = state;
 })();
--- wwwroot/js/skills/list.js
+++ wwwroot/js/skills/list.js
@@ -1,28 +1,84 @@
+window.application = window.application || {};
+window.application.skillsList = window.application.skillsList || {};
+    
+// View
+(function() {
+    var htmlNodes = {
+        keywords : $('#skills-keywords'),
+        loader : $('#skills-loader'),
+        list : $('#skills-list')
+    };
+    var utils = window.application.utils;
+
+    function update(state) {
+        for (var key in update) {
+            var updater = update[key];
+            updater(state);
+        }
+    }
+
+    update.skills = function (state) {
+        utils.fillList(htmlNodes.list, state.skills, {
+            elementDrawer: function (skill) {
+                return '<li class="list-group-item"><a class="reset" href="/skills/details?id=' + skill.Id + '">' + skill.Name + '</a></li>';
+            },
+            noResultsHtml: '<i>No skills found</i>'
+        });
+    };
+
+    update.keywords = function (state) {
+        htmlNodes.keywords.val(state.keywords);
+    };
+
+    window.application.skillsList.htmlNodes = htmlNodes;
+    window.application.skillsList.update = update;
+})();
+
+// Actions
 (function() {
     var ajax = window.application.ajax;
+    var utils = window.application.utils;
+    var htmlNodes = window.application.skillsList.htmlNodes;
+    var update = window.application.skillsList.update;
+
+    function attachEvents(state) {
+        htmlNodes.keywords.on('keyup', utils.eventDelayer(utils.eventLinker(getSkills, state)));
+        $().ready(utils.eventLinker(initializeView, state));
+    }
+
+    function getSkills(state, event) {
+        state.keywords = event.target.value;
+        _loadSkills(state);
+    }
+
+    function initializeView(state, event) {
+        _loadSkills(state);
+    }
+
+    function _loadSkills(state) {
+        utils.longOperation(skillsPromise, htmlNodes.loader);
+
+        function skillsPromise() {
+            return ajax.get('/api/skill?keywords=' + state.keywords + '&page=' + state.page + '&pageSize=' + state.pageSize, [])
+            .then(function(skills) {
+                state.skills = skills;
+                update.skills(state);
+            });
+        }
+    }
+
+    window.application.skillsList.attachEvents = attachEvents;
+})();
+
+// Model
+(function() {
     var state = {
         keywords: '',
         page: 0,
         pageSize: 10,
         skills: []
     };
-    var searchList = new window.application.SearchList('skills', skillsPromise, {
-        elementDrawer: skillsDrawer,
-        noResultsHtml: '<i>No skills found</i>'
-    });
 
-    searchList.reload();
-
-    function skillsDrawer(skill) {
-        return '<li class="list-group-item"><a class="reset" href="/skills/details?id=' + skill.Id + '">' + skill.Name + '</a></li>';
-    }
-
-    function skillsPromise(keywords) {
-        state.keywords = keywords;
-        return ajax.get('/api/skill?keywords=' + state.keywords + '&page=' + state.page + '&pageSize=' + state.pageSize, [])
-        .then(function(skills) {
-            state.skills = skills;
-            return skills;
-        });
-    }
+    window.application.skillsList.attachEvents(state);
+    window.application.skillsList.state = state;
 })();
