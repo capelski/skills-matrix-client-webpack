1222affd4b6e8afad65d1bb2d5d8e99cb3e10ea2
Carles Capellas <carlesc@byrom.plc.uk>
2017-07-14 18:32:17 +0200

    Restore redux plugins (files, bower, script links). Restore redux home.js

--- wwwroot/js/home/home.js
+++ wwwroot/js/home/home.js
@@ -1,56 +1,96 @@
 (function(js, ajax, paginatedList) {
 
-    var htmlNodes = {
-        employeesList:  paginatedList.getHtmlNodes('employees'),
-        skillsList:  paginatedList.getHtmlNodes('skills')
-    };
-
-    var state = {
-        employees: paginatedList.getState(),
-        skills: paginatedList.getState()
-    };
-
-    function render() {
-        // State would be retrieved from the store in Redux
-        paginatedList.render(htmlNodes.employeesList, state.employees, {
-            elementDrawer: function (employee) {
-                return '<li class="list-group-item"><a class="reset" href="/employees/details?id=' + employee.Id + '">' + employee.Name +
-                '<span class="badge floating">' + employee.Skills.length + '</span></a></li>';
-            },
-            noResultsHtml: '<i>No employees found</i>'
-        });
-        paginatedList.render(htmlNodes.skillsList, state.skills, {
-            elementDrawer: function (skill) {
-                return '<li class="list-group-item"><a class="reset" href="/skills/details?id=' + skill.Id + '">' + skill.Name +
-                '<span class="badge floating">' + skill.Employees.length + '</span></a></li>';
-            },
-            noResultsHtml: '<i>No skills found</i>'
-        });
-    }
-
-    //Actions
-    function initialize(state) {
-        state.employees.loadPhase = 'loading';
-        state.skills.loadPhase = 'loading';
-        render();
-
+    function loadEmployees(state, dispatch) {
         js.stallPromise(ajax.get('/api/employee/getMostSkilled', {}, []), 1500)
         .then(function(employees) {
-            state.employees.loadPhase = 'loaded';
-            state.employees.results = employees;
-            render();
-        });
-
-        js.stallPromise(ajax.get('/api/skill/getRearest', {}, []), 1500)
-        .then(function(skills) {
-            state.skills.loadPhase = 'loaded';
-            state.skills.results = skills;
-            render();
+            dispatch({
+                target: 'employees',
+                paginatedList: {
+                    Items: employees,
+                    TotalPages: 0
+                },
+                type: 'updateResults'
+            });
         });
     }
 
-    $().ready(function(event) {
-        initialize(state);
+    function loadSkills(state, dispatch) {
+        js.stallPromise(ajax.get('/api/skill/getRearest', {}, []), 1500)
+        .then(function(skills) {
+            dispatch({
+                target: 'skills',
+                paginatedList: {
+                    Items: skills,
+                    TotalPages: 0
+                },
+                type: 'updateResults'
+            });
+        });
+    }
+
+    function Home(options) {
+        var self = this;
+        self.htmlNodes = options.htmlNodes;
+        self.store = options.store;
+        self.initialize = function() {
+            self.store.dispatch(function(dispatch) {
+                dispatch({
+                    target: 'employees|skills',
+                    type: 'initializeList'
+                });
+                var state = self.store.getState();
+                loadEmployees(state.employeesList, dispatch);
+                loadSkills(state.skillsList, dispatch);
+            });
+        };
+
+        function render() {
+            var state = self.store.getState();
+            paginatedList.render(self.htmlNodes.employeesList, state.employeesList, {
+                elementDrawer: function (employee) {
+                    return '<li class="list-group-item"><a class="reset" href="/employees/details?id=' + employee.Id + '">' + employee.Name +
+                    '<span class="badge floating">' + employee.Skills.length + '</span></a></li>';
+                },
+                noResultsHtml: '<i>No employees found</i>'
+            });
+            paginatedList.render(self.htmlNodes.skillsList, state.skillsList, {
+                elementDrawer: function (skill) {
+                    return '<li class="list-group-item"><a class="reset" href="/skills/details?id=' + skill.Id + '">' + skill.Name +
+                    '<span class="badge floating">' + skill.Employees.length + '</span></a></li>';
+                },
+                noResultsHtml: '<i>No skills found</i>'
+            });
+        };
+
+        self.store.subscribe(render);
+    }
+
+    function reducer(state, action) {
+        if (typeof state == "undefined") {
+            state = {};
+        }
+
+        if (action.target && action.target.indexOf('employees') > -1) {
+            state.employeesList = paginatedList.reducer(state.employeesList, action);
+        }
+        if (action.target && action.target.indexOf('skills') > -1) {
+            state.skillsList = paginatedList.reducer(state.skillsList, action);
+        }
+
+        return state;
+    }
+
+    var store = Redux.createStore(reducer, Redux.applyMiddleware(thunk));
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var home = new Home({
+            htmlNodes: {
+                employeesList: paginatedList.getHtmlNodes('employees'),
+                skillsList: paginatedList.getHtmlNodes('skills')
+            },
+            store: store
+        });
+        home.initialize();
     });
 
 })(window.JsCommons, window.Ajax, window.PaginatedList);
--- wwwroot/js/utils/paginated-list.js
+++ wwwroot/js/utils/paginated-list.js
@@ -103,6 +103,58 @@
         return state;
     };
 
+    PaginatedList.reducer = function (state, action) {
+        if (typeof state === 'undefined') {
+            state = PaginatedList.getState();
+        }
+
+        switch (action.type) {
+            case 'pageButtons':
+                if (!isNaN(action.pageAction)) {
+                    state.page = parseInt(action.pageAction);
+                }
+                else if (action.pageAction === 'previous') {
+                    if ((state.pageOffset - state.pagesNumber) >= 0) {
+                        state.pageOffset -= state.pagesNumber;                    
+                    }
+                    state.page = 0;
+                }
+                else if (action.pageAction === 'following') {
+                    if ((state.pageOffset + state.pagesNumber) < state.totalPages) {
+                        state.pageOffset += state.pagesNumber;
+                    }
+                    state.page = 0;
+                }
+                state.loadPhase = 'loading';
+                return state;
+            case 'pageSizeList':
+                state.pageSize = action.pageSize;
+                state.page = 0;
+                state.pageOffset = 0;
+                state.loadPhase = 'loading';
+                return state;
+            case 'searcher':
+                state.keywords = action.keywords;
+                state.page = 0;
+                state.pageOffset = 0;
+                state.loadPhase = 'loading';
+                return state;
+            case 'initializeList':
+                state.keywords = '';
+                state.page = 0;
+                state.pageOffset = 0;
+                state.loadPhase = 'loading';
+                return state;
+            case 'updateResults':
+                state.results = action.paginatedList.Items;
+                state.totalPages = action.paginatedList.TotalPages;
+                state.loadPhase = 'loaded';
+                return state;
+            default:
+                return state;
+        }
+    };
+
     PaginatedList.render = function(htmlNodes, state, options) {
         options = options || {};
         options.noResultsHtml = options.noResultsHtml || '<i>No results found</i>';
