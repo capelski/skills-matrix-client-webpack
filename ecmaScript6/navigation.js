e71e27f4fd6a99464498f4852ca7c2b1f600aed8
Carles Capellas <capellas.carles@gmail.com>
2017-09-25 12:18:02 +0200

    Add generic error page. Modify navigation.js to include a Go back functionality

--- ecmaScript6/navigation.js
+++ ecmaScript6/navigation.js
@@ -1,19 +1,51 @@
 (function (js, store, views) {
 
-    function navigate(htmlNodeId, viewData) {
-        store.dispatch({
-            type: 'loading',
-            reason: 'navigation-started'
-        });
-        var view = views.find(view => view.htmlNodeId == htmlNodeId);
-        js.stallPromise(view.loader(viewData, store), 500)
+    var loadMinimumTime = 500;
+    var previousPage;
+
+    function _navigate(view, viewData) {
+        js.stallPromise(view.loader(viewData, store), loadMinimumTime)
         .then(() => {
             store.dispatch({
                 type: 'loaded',
                 reason: 'navigation-finished',
-                htmlNodeId
+                htmlNodeId: view.htmlNodeId
             });
         });
+    }
+
+    function _navigationError() {
+        setTimeout(() => {
+            store.dispatch({
+                type: 'loaded',
+                reason: 'navigation-finished',
+                htmlNodeId: 'error-page'
+            });
+        }, loadMinimumTime);
+    }
+
+    function navigate(htmlNodeId, viewData) {
+
+        store.dispatch({
+            type: 'loading',
+            reason: 'navigation-started'
+        });
+
+        if (htmlNodeId == '_previous' && previousPage) {
+            return _navigate(previousPage.view, previousPage.viewData);
+        }
+        
+        var view = views.find(view => view.htmlNodeId == htmlNodeId);
+        if (view) {
+            _navigate(view, viewData);
+            previousPage = {
+                view,
+                viewData
+            };
+        }
+        else {
+            _navigationError();
+        }
     };
 
     window.Navigate = navigate;
