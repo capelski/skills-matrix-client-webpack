ec75b9d63a34a0a67c8e04bc12c857b72cbfe640
Carles Capellas <capellas.carles@gmail.com>
2017-09-26 22:31:09 +0200

    Modify application structure and webpack.config.js to copy static files on build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-const webpack = require('webpack'); //to access built-in plugins
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	entry: './source/index.js',
+	entry: './source/js/index.js',
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'distribution'),
 		filename: 'main.js'
 	},
 	module: {
@@ -19,7 +20,21 @@ module.exports = {
 		}]
 	},
 	plugins: [
-		//new webpack.optimize.UglifyJsPlugin(),
-		new HtmlWebpackPlugin({template: './source/index.html'})
+		new webpack.optimize.UglifyJsPlugin(),
+		new HtmlWebpackPlugin({template: './source/index.html'}),
+		new CopyWebpackPlugin([{
+			from: './source/favicon.ico',
+			to: 'favicon.ico'
+		}, {
+			from: './source/css',
+			to: 'css/[name].[ext]'
+		}, {
+			from: './source/images',
+			to: 'images/[name].[ext]'
+		}, {
+            context: './source/plugins',
+            from: '**/*',
+            to: './plugins'
+        }])
 	]
 };
